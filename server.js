6c3d5f4f371e61f4d46b14495d1be0fd36c26add
Phina-d <phinadiouf@gmail.com>
2025-07-08 21:29:55 +0000

    mise à jour

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   // Définit le code de réponse HTTP (200 OK) et le type de contenu (HTML)
   res.writeHead(200, { 'Content-Type': 'text/html' });
+
   // Envoie une réponse HTML au client
   res.end('<h1>Bonjour Node !!!!</h1>\n');
 });
